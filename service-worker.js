e4c95955a3e857dc77a7e098573bb2e763a10f55
Rory Farrell <raz.our2gems@gmail.com>
2025-08-14 18:49:14 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,50 +1,29 @@
-const CACHE_NAME = 'dbt-skills-v1';
+const cacheName = 'dbt-game-v1';
 const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  'https://cdn.tailwindcss.com',
-  // You may need to add other assets if you add more
-  // images, fonts, or external scripts.
+  'https://cdn.tailwindcss.com'
 ];
 
-// Install event: cache all necessary files
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
+    caches.open(cacheName)
+      .then(cache => {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-// Fetch event: serve cached content when offline
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
+      .then(response => {
         if (response) {
           return response;
         }
         return fetch(event.request);
       })
-  );
-});
-
-// Activate event: clean up old caches
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
   );
 });
